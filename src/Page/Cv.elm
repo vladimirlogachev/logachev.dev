50ce2cadffd1c46330aac1b4aa3296b191f8d99b
Vladimir Logachev <logachev.dev@ya.ru>
2022-01-15 09:11:53 +0700

    Upd

--- src/Page/Cv.elm
+++ src/Page/Cv.elm
@@ -17,7 +17,7 @@ bioSection =
         , preparedParagraph "I like to see functional programming deliver value to both companies and individual specialists, so I devote a lot of time and attention not only to code but also to people. I'm a big fan of meetups and reading groups, which I run at my workplaces from time to time, and also I consider pair programming and pair testing to be very effective practices."
         , preparedParagraph "In addition to technical experience, I have entrepreneurial experience, so on my personal projects, I act without a need for instructions, come up with ideas, fill in the missing pieces myself, produce an immediate result, and apply the polishing later."
         , preparedParagraph "That's why I prefer functional languages that implement strict static typing, such as Haskell and Elm. My preference is based on the idea that once we have strict static typing and ADTs, we can move very quickly without fear of making design mistakes because they become easy to solve later. (And we can move even more quickly with the help of code generation and mono repository)."
-        , preparedParagraph "As a person, I love the win-win approach, I don't play a victim, I don't seek approval and publically respect everyone who helps others or has knowledge. So, the vision of my technical career is that FP and strong teamwork can make everyone involved rich."
+        , preparedParagraph "As a person, I love the win-win approach, I don't play a victim, I don't seek approval and I do publically respect everyone who helps others or has knowledge. So, the vision of my technical career is that FP and strong teamwork can make everyone involved rich."
         ]
 
 
