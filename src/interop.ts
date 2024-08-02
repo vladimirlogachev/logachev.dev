faabad2a47b22dfdd8783ddb60124ede5a7ffcf7
Vladimir Logachev <vladimir@logachev.dev>
2024-08-02 15:44:35 +0400

    Add comment

--- src/interop.ts
+++ src/interop.ts
@@ -9,6 +9,7 @@ export const flags = ({ env }) => {
 
 export const onReady = ({ app, env }) => {
   // Note: Reset scroll to page top when navigating between SPA pages (otherwise there will be some weird scroll jumps)
+  // IMPORTANT: The port must be defined in Elm code as well, otherwise the app will go insane without visible errors.
   app.ports.urlChanged.subscribe((val) => {
     window.scrollTo(0, 0);
   });
